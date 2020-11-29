36214f7d421d322100ff8d45b3a62506ac3b6c57
Chris Frey <christopher.r.frey1@gmail.com>
2020-11-29 15:55:05 -0700

    Population, all stats, and strength/intelligence stat analysis implemented for total population and species.

--- src/statistics_framework.py
+++ src/statistics_framework.py
@@ -53,7 +53,26 @@ def DrawHist(data_set, title, yLabel):
     plt.subplots_adjust(bottom=0.15)
     plt.show()
 
-def population_time(frame):
+
+def total_population_time(frame):
+    x = np.zeros(frame['ticks'][0])
+    y = np.zeros(frame['ticks'][0])
+    for i in range(x.size):
+        x[i] = i
+        for j in range(len(frame.index)):
+            age = frame['age'][j]
+            birth = frame['birth_tick'][j]
+            # If alive at time tick
+            if birth <= i and age + birth+1 >= i: 
+                y[i] += 1
+    fig, ax = plt.subplots()
+    ax.plot(x, y, label='Total Population')
+    ax.grid()
+    ax.legend()
+    ax.set(xlabel='Time Ticks', ylabel='Population', title='Total Population/Time')
+    plt.show()
+
+def species_population_time(frame):
     x = np.zeros(frame['ticks'][0])
     y_tot = np.zeros(frame['ticks'][0])
     y_evil = np.zeros(frame['ticks'][0])
@@ -79,7 +98,7 @@ def population_time(frame):
     ax.legend()
     plt.show()
 
-def stats_time(frame):
+def total_stats_time(frame):
     x = np.zeros(frame['ticks'][0])
     speed = np.zeros(frame['ticks'][0])
     agility = np.zeros(frame['ticks'][0])
@@ -91,11 +110,13 @@ def stats_time(frame):
 
     for i in range(x.size):
         x[i] = i
+        num_alive = 0
         for j in range(len(frame.index)):
             age = frame['age'][j]
             birth = frame['birth_tick'][j]
             # If alive at time tick
             if birth <= i and age + birth+1 >= i:
+                num_alive += 1
                 speed[i] += frame['speed'][j]
                 agility[i] += frame['agility'][j]
                 intelligence[i] += frame['intelligence'][j]
@@ -103,26 +124,162 @@ def stats_time(frame):
                 strength[i] += frame['strength'][j]
                 fertility[i] += frame['fertility'][j]
                 bite_size[i] += frame['bite_size'][j]
+        if num_alive != 0:
+            speed[i] /= num_alive
+            agility[i] /= num_alive
+            intelligence[i] /= num_alive
+            endurance[i] /= num_alive
+            strength[i] /= num_alive
+            fertility[i] /= num_alive
+            bite_size[i] /= num_alive
+
+    fig, ax = plt.subplots()
+    ax.plot(x, speed, label='Speed Average')
+    ax.plot(x, agility, label='Agility Average')
+    ax.plot(x, intelligence, label='Intelligence Average')
+    ax.plot(x, endurance, label='Endurance Average')
+    ax.plot(x, strength, label='Strength Average')
+    ax.plot(x, fertility, label='Fertility Average')
+    ax.plot(x, bite_size, label='Bite Size Average')
+    ax.grid()
+    ax.set(xlabel='Time Ticks', ylabel='Average Stat Totals', title='Total Stats Average/Time')
+    ax.legend()
+    plt.show()
+
+def species_stats_time(frame):
+    x = np.zeros(frame['ticks'][0])
+    evil_speed = np.zeros(frame['ticks'][0])
+    evil_agility = np.zeros(frame['ticks'][0])
+    evil_intelligence = np.zeros(frame['ticks'][0])
+    evil_endurance = np.zeros(frame['ticks'][0])
+    evil_strength = np.zeros(frame['ticks'][0])
+    evil_fertility = np.zeros(frame['ticks'][0])
+    evil_bite_size = np.zeros(frame['ticks'][0])
+
+    neutral_speed = np.zeros(frame['ticks'][0])
+    neutral_agility = np.zeros(frame['ticks'][0])
+    neutral_intelligence = np.zeros(frame['ticks'][0])
+    neutral_endurance = np.zeros(frame['ticks'][0])
+    neutral_strength = np.zeros(frame['ticks'][0])
+    neutral_fertility = np.zeros(frame['ticks'][0])
+    neutral_bite_size = np.zeros(frame['ticks'][0])
+
+    for i in range(x.size):
+        x[i] = i
+        evil_alive = 0
+        neutral_alive = 0
+        for j in range(len(frame.index)):
+            age = frame['age'][j]
+            birth = frame['birth_tick'][j]
+            # If alive at time tick
+            if birth <= i and age + birth+1 >= i:
+                if frame['type'][j] == 'ObjectType.NEUTRAL':
+                    neutral_alive += 1
+                    neutral_speed[i] += frame['speed'][j]
+                    neutral_agility[i] += frame['agility'][j]
+                    neutral_intelligence[i] += frame['intelligence'][j]
+                    neutral_endurance[i] += frame['endurance'][j]
+                    neutral_strength[i] += frame['strength'][j]
+                    neutral_fertility[i] += frame['fertility'][j]
+                    neutral_bite_size[i] += frame['bite_size'][j]
+                else:
+                    evil_alive += 1
+                    evil_speed[i] += frame['speed'][j]
+                    evil_agility[i] += frame['agility'][j]
+                    evil_intelligence[i] += frame['intelligence'][j]
+                    evil_endurance[i] += frame['endurance'][j]
+                    evil_strength[i] += frame['strength'][j]
+                    evil_fertility[i] += frame['fertility'][j]
+                    evil_bite_size[i] += frame['bite_size'][j]
+        if neutral_alive != 0:
+            neutral_speed[i] /= neutral_alive
+            neutral_agility[i] /= neutral_alive
+            neutral_intelligence[i] /= neutral_alive
+            neutral_endurance[i] /= neutral_alive
+            neutral_strength[i] /= neutral_alive
+            neutral_fertility[i] /= neutral_alive
+            neutral_bite_size[i] /= neutral_alive
+        if evil_alive != 0:
+            evil_speed[i] /= evil_alive
+            evil_agility[i] /= evil_alive
+            evil_intelligence[i] /= evil_alive
+            evil_endurance[i] /= evil_alive
+            evil_strength[i] /= evil_alive
+            evil_fertility[i] /= evil_alive
+            evil_bite_size[i] /= evil_alive
     
     fig, ax = plt.subplots()
-    ax.plot(x, speed, label='Speed')
-    ax.plot(x, agility, label='Agility')
-    ax.plot(x, intelligence, label='Intelligence')
-    ax.plot(x, endurance, label='Endurance')
-    ax.plot(x, strength, label='Strength')
-    ax.plot(x, fertility, label='Fertility')
-    ax.plot(x, bite_size, label='Bite Size')
+    ax.plot(x, evil_speed, 'b-', label='Average Carnivore Speed')
+    ax.plot(x, evil_agility, 'g-', label='Average Carnivore Agility')
+    ax.plot(x, evil_intelligence, 'r-', label='Average Carnivore Intelligence')
+    ax.plot(x, evil_endurance, 'c-', label='Average Carnivore Endurance')
+    ax.plot(x, evil_strength, 'm-', label='Average Carnivore Strength')
+    ax.plot(x, evil_fertility, 'y-', label='Average Carnivore Fertility')
+    ax.plot(x, evil_bite_size, 'k-', label='Average Carnivore Bite Size')
+
+    ax.plot(x, neutral_speed, 'b--', label='Average Herbivore Speed')
+    ax.plot(x, neutral_agility, 'g--', label='Average Herbivore Agility')
+    ax.plot(x, neutral_intelligence, 'r--', label='Average Herbivore Intelligence')
+    ax.plot(x, neutral_endurance, 'c--', label='Average Herbivore Endurance')
+    ax.plot(x, neutral_strength, 'm--', label='Average Herbivore Strength')
+    ax.plot(x, neutral_fertility, 'y--', label='Average Herbivore Fertility')
+    ax.plot(x, neutral_bite_size, 'k--', label='Average Herbivore Bite Size')
     ax.grid()
-    ax.set(xlabel='Time Ticks', ylabel='Stat Totals', title='Stats/Time')
     ax.legend()
+    ax.set(xlabel='Time Ticks', ylabel='Average Species Stat Totals', title='Average Species Stats/Time')
+    plt.show()
+
+def species_strength_intel_time(frame):
+    x = np.zeros(frame['ticks'][0])
+    evil_intelligence = np.zeros(frame['ticks'][0])
+    evil_strength = np.zeros(frame['ticks'][0])
+
+    neutral_intelligence = np.zeros(frame['ticks'][0])
+    neutral_strength = np.zeros(frame['ticks'][0])
+
+    for i in range(x.size):
+        x[i] = i
+        evil_alive = 0
+        neutral_alive = 0
+        for j in range(len(frame.index)):
+            age = frame['age'][j]
+            birth = frame['birth_tick'][j]
+            # If alive at time tick
+            if birth <= i and age + birth+1 >= i:
+                if frame['type'][j] == 'ObjectType.NEUTRAL':
+                    neutral_intelligence[i] += frame['intelligence'][j]
+                    neutral_strength[i] += frame['strength'][j]
+                else:
+                    evil_intelligence[i] += frame['intelligence'][j]
+                    evil_strength[i] += frame['strength'][j]
+        if neutral_alive != 0:
+            neutral_intelligence[i] /= neutral_alive
+            neutral_strength[i] /= neutral_alive
+        if evil_alive != 0:
+            evil_intelligence[i] /= evil_alive
+            evil_strength[i] /= evil_alive
+    
+    fig, ax = plt.subplots()
+    ax.plot(x, evil_intelligence, 'r-', label='Evil Average Intelligence')
+    ax.plot(x, evil_strength, 'm-', label='Evil Average Strength')
+
+    ax.plot(x, neutral_intelligence, 'r--', label='Neutral Average Intelligence')
+    ax.plot(x, neutral_strength, 'm--', label='Neutral Average Strength')
+    ax.grid()
+    ax.legend()
+    ax.set(xlabel='Time Ticks', ylabel='Strength & Intelligence', title='Strength & Intelligence/Time')
     plt.show()
 
 def run_analysis():
     frame = read_data('agent_data.csv')
     herb_frame = read_data('herb_agent_data.csv')
     carn_frame = read_data('carn_agent_data.csv')
-    population_time(frame)
-    stats_time(frame)
+    # total_population_time(frame)
+    # species_population_time(frame)
+    # total_stats_time(frame)
+    # species_stats_time(frame)
+    #species_strength_intel_time(frame)
+
     
     if frame is not None:
         DrawHist(frame.loc[:, 'health'], "Health of Buddies", "Number of Buddies")
